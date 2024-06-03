99278835cafb09a5511079f99d95f5c654d57d3f
Hiroki Yoshitake <hiroki.yoshitak@gmail.com>
2024-06-04 03:17:27 +0900

    スマホ対応/発言をおしゃれなデザインに変更

--- app/public/js/socket.js
+++ app/public/js/socket.js
@@ -6,10 +6,12 @@ const params = url.searchParams;
 const urlRoomId = parseInt(params.get('roomId'));
 const urlName = params.get('name');
 
+// ルームに入る
 socket.on("connect", () => {
   socket.emit("join", { roomId: urlRoomId, name: urlName });
 })
 
+// 発言する
 document.getElementById("frm-post").addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -18,29 +20,50 @@ document.getElementById("frm-post").addEventListener("submit", (e) => {
     return false;
   }
 
-  socket.emit("post", { name: urlName, text: msg.value });
+  socket.emit("post", { name: urlName, text: msg.value, time: new Date()});
 
+  // メッセージを送信したら、入力欄を空にする
   msg.value = "";
 });
 
+// メッセージを受信する
 socket.on("message", (msg) => {
   const list = document.getElementById("msglist");
-  const li = document.createElement("li");
-  li.innerHTML = `${msg.name}:${msg.text}`;
-  list.insertBefore(li, list.firstChild);
+  const commentDom = document.createElement("div");
 
-  li.classList.add('animate__animated', 'animate__fadeInDown');
-  party.confetti(li);
-  // 10回繰り返す
-  for (let i = 0; i < 15; i++) {
+  // 時間をhh:mm:ssにフォーマットする
+  const msgTime = dayjs(msg.time)
+  const msgTimeFormat = msgTime.format('hh:mm:ss')
+
+  // 受診したメッセージをDOMにセットする
+  commentDom.innerHTML =
+    `
+    <div class="faceicon">
+      <div class="name">${msg.name}</div>
+      <div class="time">${msgTimeFormat}</div>
+    </div>
+    <div class="chatting">
+      <div class="says">
+        <p>${msg.text}</p>
+      </div>
+    </div>`
+  list.insertBefore(commentDom, list.firstChild);
+
+  // フェードインするアニメーションを付ける
+  commentDom.classList.add('animate__animated', 'animate__fadeInDown');
+
+  // 紙吹雪を降らせる
+  for (let i = 0; i < 7; i++) {
     setTimeout(function () {
-      party.confetti(li);
+      party.confetti(document.body);
     }, i * 200);
   }
-
-
 });
 
+// 画面が表示したら、入力欄にフォーカスする
 window.addEventListener('load', () => {
   msg.focus();
+  // URLパラメータをDOMにセットする
+  document.getElementById("roomid").innerHTML = `ルームID: ${urlRoomId}`;
+  document.getElementById("name").innerHTML = `名前: ${urlName}`;
 })
