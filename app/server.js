2799395b9fc2fb0a68d7f32481ae535f06f786ae
mcfyoshitake <yoshitake.hiroki@layered.inc>
2024-12-11 01:40:09 +0900

    cors対応

--- app/server.js
+++ app/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:13000",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 let store = {};
 
