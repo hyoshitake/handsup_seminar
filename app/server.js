8ebe4fb52144f153fd7558c72858e89ea173c013
mcfyoshitake <yoshitake.hiroki@layered.inc>
2024-12-19 18:56:08 +0900

    corsを追加

--- app/server.js
+++ app/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://docs.google.com",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
 
 let store = {};
 
